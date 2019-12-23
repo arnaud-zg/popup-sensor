4f8265efb4e83befe3c7551f0b00de911cb5ebef
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-12-23 23:54:03 +0100

    fix: move enum to types folder (#11)

--- src/constant/index.ts
+++
@@ -1,4 +0,0 @@
-export enum EPopupAction {
-  NONE = 'NONE',
-  CLOSE = 'CLOSE',
-}
--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,7 @@
-import { EPopupAction } from '../src/constant'
+export enum EPopupAction {
+  NONE = 'NONE',
+  CLOSE = 'CLOSE',
+}
 
 export type TOnClose = Function
 export type TOnLocationChange = (location: Location) => EPopupAction
